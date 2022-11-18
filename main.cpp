4a8a9b621eff28efbe6c8cfd4f9f7afcc92b1503
DESKTOP-O4BPLA5\Mattia Sacchi <mattiasacchi2002@gmail.com>
2022-11-18 14:24:56 +0100

    Style

--- main.cpp
+++ main.cpp
@@ -6,6 +6,10 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MW w;
+    QFile file(":/QTDark.stylesheet");
+    file.open(QIODevice::ReadOnly);
+
+    w.setStyleSheet(QString::fromUtf8(file.readAll()));
     w.show();
     return a.exec();
 }
